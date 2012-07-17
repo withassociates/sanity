4010e0e2765c5919d32a63452ac24deb807d6f25
Jamie White <jamie@jgwhite.co.uk>
2012-07-17 11:38:45 +0100

    Add rspec and sanity_spec

---
+++ sanity_spec.rb
@@ -0,0 +1,13 @@
+describe "A happy site" do
+  it "has a meaningful title"
+  it "has a meaningful description"
+  it "has open graph tags"
+  it "has twitter card tags"
+  it "has google analytics"
+  it "has a favicon"
+  it "has a touch icon"
+  it "has an appropriate humans.txt"
+  it "has an appropriate robots.txt"
+  it "has a helpful 404 page"
+  it "has a helpful 500 page"
+end
