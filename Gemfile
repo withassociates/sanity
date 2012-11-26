1b8b8af3fe016f00209ea4cb5517e08a1c24332b
Jamie White <jamie@jgwhite.co.uk>
2012-11-26 18:16:09 +0000

    :)

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source :rubygems
-gem 'poltergeist'
 gem 'rspec'
+gem 'pry'
--- sanity_spec.rb
+++ sanity_spec.rb
@@ -1,48 +1,60 @@
 require 'bundler/setup'
 require 'rspec'
-require 'capybara/rspec'
-require 'capybara/dsl'
-require 'capybara/poltergeist'
-
-Capybara.default_driver = :poltergeist
-Capybara.run_server = false
-Capybara.app_host = 'http://2012.withassociates.com'
-
-RSpec.configure do |config|
-  config.include Capybara::DSL
-  config.order = :random
-end
+require 'open-uri'
+require 'pry'
 
 describe "A happy site" do
-  before do
-    visit '/'
+  let :base do
+    'http://2012.withassociates.com'
+  end
+
+  let :homepage do
+    open "#{base}/"
   end
 
   it "has a meaningful title" do
-    page.should have_css 'title', :text => 'With Associates'
+    homepage.read.should include '<title>With Associates</title>'
   end
 
   it "has a meaningful description" do
-    page.should have_css 'meta[name="description"]'
+    homepage.read.should include '<meta name="description"'
   end
 
   it "has open graph tags" do
-    page.should have_css 'meta[name="og:title"]'
+    homepage.read.should include '<meta name="og:title"'
   end
 
   it "has twitter card tags" do
-    page.should have_css 'name[name="twitter:site]'
+    homepage.read.should include '<meta name="twitter:site"'
   end
 
   it "has google analytics" do
-    page.body.should include '_gaq'
+    homepage.read.should include '_gaq'
   end
 
-  it "has a favicon"
-  it "has a touch icon"
-  it "has an appropriate humans.txt"
-  it "has an appropriate robots.txt"
-  it "has a helpful 404 page"
-  it "has a helpful 500 page"
+  it "has a favicon" do
+    open("#{base}/favicon.ico").status.should == %w[200 OK]
+  end
+
+  it "has a touch icon" do
+    homepage.read.should include '<link rel="apple-touch-icon"'
+  end
+
+  it "has an appropriate humans.txt" do
+    open("#{base}/humans.txt").status.should == %w[200 OK]
+  end
+
+  it "has an appropriate robots.txt" do
+    open("#{base}/robots.txt").status.should == %w[200 OK]
+  end
+
+  it "has a helpful 404 page" do
+    open("#{base}/404.html").read.should include '404'
+  end
+
+  it "has a helpful 500 page" do
+    open("#{base}/500.html").read.should include '500'
+  end
+
   it "has all its domains registered with the font provider"
 end
