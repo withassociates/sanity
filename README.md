b43c82583f3de72d297a3c972346609c53faf233
Jamie White <jamie@jgwhite.co.uk>
2012-11-26 17:36:04 +0000

    Capybara-style specs

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ sanity
 
 An automated sanity check, intended to catch common errors on production sites.
 
+To run, type `ruby sanity_spec.rb`.
+
 To add a new check, edit the `sanity_spec.rb`, using other checks as a guideline.
--- sanity_spec.rb
+++ sanity_spec.rb
@@ -1,9 +1,43 @@
+require 'bundler/setup'
+require 'rspec'
+require 'capybara/rspec'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+
+Capybara.default_driver = :poltergeist
+Capybara.run_server = false
+Capybara.app_host = 'http://2012.withassociates.com'
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.order = :random
+end
+
 describe "A happy site" do
-  it "has a meaningful title"
-  it "has a meaningful description"
-  it "has open graph tags"
-  it "has twitter card tags"
-  it "has google analytics"
+  before do
+    visit '/'
+  end
+
+  it "has a meaningful title" do
+    page.should have_css 'title', :text => 'With Associates'
+  end
+
+  it "has a meaningful description" do
+    page.should have_css 'meta[name="description"]'
+  end
+
+  it "has open graph tags" do
+    page.should have_css 'meta[name="og:title"]'
+  end
+
+  it "has twitter card tags" do
+    page.should have_css 'name[name="twitter:site]'
+  end
+
+  it "has google analytics" do
+    page.body.should include '_gaq'
+  end
+
   it "has a favicon"
   it "has a touch icon"
   it "has an appropriate humans.txt"
